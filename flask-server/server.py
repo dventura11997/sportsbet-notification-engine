b996e62081a0bb43e336aa32a4cd6a872d810aaf
dventura11997 <mergyventura495@gmail.com>
2025-03-23 23:19:40 +1100

    get rid of db.create_all()

--- flask-server/server.py
+++ flask-server/server.py
@@ -53,7 +53,7 @@ def send_email():
     
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
 
